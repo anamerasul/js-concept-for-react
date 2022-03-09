d80006d13fb0207e4e1fc4a6ac9476ed424954d8
anamerasul <anamerasul@gmail.com>
2022-03-09 17:10:11 +0600

    Module summary Function এর default parameter

---
+++ sumarry.js
@@ -0,0 +1,57 @@
+/* 
+
+fundamentals
+1.variable (let,const) three types of variable
+
+1.2 condition (<,>,===,!==,<=,>=),&&,||,if,else else if
+
+1.3 array :declare,length,index,push, pop,indexOf,includes,
+1.4 for loop,while,
+
+1.5 function return parameter,
+1.6 object property,including array,object.
+-----------------\
+
+ES6
+1.template string
+
+2.spread oparetor (...)copy an array an add a new new element,
+
+use filter to remove an element from an array
+
+3.arrow function
+
+no paramerter,
+single parameter, multiple  parameter
+
+multiline,
+
+destrucing :
+--object and array
+5.object short hand 
+6 function parameter default value
+7 optional chaning  .?
+------
+Browser API
+
+1.local storage session storage
+2 location API
+3 HISTORY API
+4.fetch
+-----
+others array: map,forEach,filter,find,
+ternary Operator 
+
+logical and logical or && ||
+JSON (stringify,parse)
+
+
+*/
+
+const x=50;
+const y=100;
+
+const obj={
+        x:x,
+        y:y,
+}
